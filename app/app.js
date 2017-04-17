34414967789a119d3330c2744f949e4adcdb2f6d
Ewelina Dziewiela <ewelinadziewiela@MacBook-Pro-Ewelina.local>
2017-04-17 22:05:13 +0200

    styles and js updates

--- app/app.js
+++ app/app.js
@@ -1,12 +1,3 @@
-//$(function(){
-//    $.getJSON('http://jsonplaceholder.typicode.com/posts', function(data) {
-//        data.forEach(function(item){
-//            addSinglePost(item);
-//
-//        })
-//    });
-//});
-
 
 $.ajax({
     type: "GET",
@@ -17,15 +8,11 @@ $.ajax({
         var limitedPosts = data.splice(0, 20);
         limitedPosts.forEach(function (item) {
             addSinglePost(item);
-
-
         });
         $(".post-btn").click(function () {
             var postId = $(this).attr("post-id");
             console.log(postId);
             getSinglePost(postId);
-
-
         });
 
     },
@@ -44,8 +31,6 @@ function getSinglePost(id) {
         success: function (data) {
             console.log(data);
             addModal(data);
-
-
         },
         error: function (err) {
             console.log("Nie ma nic...")
@@ -56,36 +41,35 @@ function getSinglePost(id) {
 
 function addSinglePost(item) {
 
-    var post = $(`<div class="col-md-6 col-sm-12 single-post">
-                    <div class="single-post__wrapper">
-                        <h4 class="post-title">${item.title}</h4>
-                        <p class="post-text">${item.body}</p>
-                        <button post-id="${item.id}" type="button" class="btn btn-info btn-lg post-btn"
-                                data-toggle="modal" data-target="#myModal">Zobacz więcej</button>
-                  </div>
-                   `);
+    var post = $('<div class="col-md-6 col-sm-12 single-post">' +
+        '<div class="single-post__wrapper">' +
+        '<h4 class="post-title">' + item.title + '</h4>' +
+        '<p class="post-text">' + item.body + '</p>' +
+        '<button post-id="' + item.id + '" type="button" class="btn btn-info btn-lg post-btn"' +
+        'data-toggle="modal" data-target="#myModal">Zobacz więcej</button>' +
+        '</div>'
+    );
 
     $('.post-list__wrapper').append(post);
 }
 
 
 function addModal(item) {
-    var modal = $(`
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title post-title">${item.id} &#9679; ${item.title}</h4>
-                </div>
-                <div class="modal-body post-text">
-                    <p>${item.body}</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default close-btn" data-dismiss="modal">Zamknij</button>
-                </div>
-            </div>
-        </div>
-        `);
+    var modal = $('<div class="modal-dialog">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
+        '<h4 class="modal-title post-title">' + item.id + ' &#9679; ' + item.title + '</h4>' +
+        '</div>' +
+        '<div class="modal-body post-text">' +
+        '<p>' + item.body + '</p>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '<button type="button" class="btn btn-default close-btn" data-dismiss="modal">Zamknij</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+    );
 
     $("#myModal").html(modal);
 
