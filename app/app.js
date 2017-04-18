c037906c25c19e0525a991c6673e3d2a00248df0
Ewelina Dziewiela <ewelinadziewiela@MacBook-Pro-Ewelina.local>
2017-04-18 08:28:30 +0200

    js - new vs old

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-
 $.ajax({
     type: "GET",
     url: "http://jsonplaceholder.typicode.com/posts",
@@ -74,3 +73,41 @@ function addModal(item) {
     $("#myModal").html(modal);
 
 }
+
+//Moja pierwsza wersja, ktora niestety nie dziala pod IE, dlatego musialam zmienic
+
+//function addSinglePost(item) {
+//
+//    var post = $(`<div class="col-md-6 col-sm-12 single-post">
+//        <div class="single-post__wrapper">
+//        <h4 class="post-title">${item.title}</h4>
+//        <p class="post-text">${item.body}</p>
+//        <button post-id="${item.id}" type="button" class="btn btn-info btn-lg post-btn"
+//        data-toggle="modal" data-target="#myModal">Zobacz więcej</button>
+//        </div>`
+//    );
+//
+//    $('.post-list__wrapper').append(post);
+//}
+//
+//
+//function addModal(item) {
+//    var modal = $(`<div class="modal-dialog">
+//        <div class="modal-content">
+//        <div class="modal-header">
+//        <button type="button" class="close" data-dismiss="modal">&times;</button>
+//        <h4 class="modal-title post-title">${item.id}  &#9679; ${item.title}</h4>
+//        </div>
+//        <div class="modal-body post-text">
+//        <p>${item.body}</p>
+//        </div>
+//        <div class="modal-footer">
+//        <button type="button" class="btn btn-default close-btn" data-dismiss="modal">Zamknij</button>
+//        </div>
+//        </div>
+//        </div>`
+//    );
+//
+//    $("#myModal").html(modal);
+//
+//}
